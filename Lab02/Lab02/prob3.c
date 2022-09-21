9dabc6bb51ed2011b0b398738b1c7be986ffc3f4
Chaehyun Park <c.park@snu.ac.kr>
2022-09-21 22:12:08 +0900

    Lab02

---
+++ Lab02/Lab02/prob3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+    
+    int num;
+    char chr;
+    
+    scanf("%d", &num);
+    getchar();
+    scanf("%c", &chr);
+    
+    printf("%c\n%d\n", num, chr);
+    
+    return 0;
+}
