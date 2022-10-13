2cc20639e65d73a66ce3c1695049962e08f165cf
Chaehyun Park <c.park@snu.ac.kr>
2022-10-13 11:45:52 +0900

    Lab05_improved
    Improvement on prob2: now prints the same combinations just once.

---
+++ Lab05/Lab05/prob2_improved.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    for(int a=1; a<=100; a++){
+        for(int b=1; b<=100; b++){
+            for(int c=1; c<=100; c++){
+                if((a<b&&b<c)&&((a*a+b*b==c*c)||(a*a+c*c==b*b)||(b*b+c*c==a*a)))
+                    printf("%d %d %d\n", a, b, c);
+            }
+        }
+    }
+    
+    return 0;
+}
